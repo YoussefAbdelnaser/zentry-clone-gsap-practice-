ef84ba7ee8495d0f622952606152cc31a9b94808
YoussefAbdelnaser <youssefabdelnasersobhy@yahoo.com>
2025-09-02 00:10:51 +0300

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,48 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+import AnimatedTitle from "./AnimatedTitle";
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm md:text-[10px]">Welcome to Zentry</h2>
+        <AnimatedTitle />
+        <div className="about-subtext">
+          <p>The Game of games begins-your life, now an epic MMORPG</p>
+          <p>Zentry Unites every player from countless games and platforms</p>
+        </div>
+      </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="bg"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AnimatedTitle = () => {
+  return (
+    <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+      <h1 className="font-zentry text-blue-200">
+        Disc<b>o</b>ver the world's <br /> most hyped pent<b>a</b>kill
+      </h1>
+    </div>
+  );
+};
+
+export default AnimatedTitle;
